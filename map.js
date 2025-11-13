96f184d4dc17453e22367b85870e04a50cc48f43
tanishaiyer1 <tiyer@ucsd.edu>
2025-11-13 15:20:35 -0800

    lat long

--- map.js
+++ map.js
@@ -1,105 +1,3 @@
-// import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
-// import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
-
-// // Check that Mapbox GL JS is loaded
-// console.log('Mapbox GL JS Loaded:', mapboxgl);
-
-// mapboxgl.accessToken = 'pk.eyJ1IjoidGl5ZXIiLCJhIjoiY21oeHhjbmNiMDYwYjJqcHV3YzZzcjdxdSJ9.NfHaJFLVUPK5zfvEVW-U-A';
-
-// const map = new mapboxgl.Map({
-//   container: 'map',
-//   style: 'mapbox://styles/mapbox/streets-v12',
-//   center: [-71.09415, 42.36027],
-//   zoom: 12,
-//   minZoom: 5,
-//   maxZoom: 18,
-// });
-
-
-// map.on('load', async () => {
-//   // Add Boston bike lanes as a source
-//   map.addSource('boston_route', {
-//     type: 'geojson',
-//     data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
-//   });
-
-//   // Add Boston bike lanes as a line layer
-//   map.addLayer({
-//     id: 'bike-lanes',
-//     type: 'line',
-//     source: 'boston_route',
-//     paint: {
-//       'line-color': 'green',
-//       'line-width': 3,
-//       'line-opacity': 0.4,
-//     }
-//   });
-//     // Add Cambridge bike lanes as a source
-//   map.addSource('cambridge_route', {
-//     type: 'geojson',
-//     data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
-//   });
-
-//   // Add Cambridge bike lanes as a line layer
-//   map.addLayer({
-//     id: 'bike-lanes-cambridge',
-//     type: 'line',
-//     source: 'cambridge_route',
-//     paint: {
-//       'line-color': 'green',
-//       'line-width': 3,
-//       'line-opacity': 0.4,
-//     }
-//   });
-
-//   const svg = d3.select('#map').select('svg');
-//   function getCoords(station) {
-//   // Note: Some files use 'lat'/'long', others use 'Lat'/'Long'
-//   const point = new mapboxgl.LngLat(+station.Long, +station.Lat);
-//   const { x, y } = map.project(point);
-//   return { cx: x, cy: y };
-// }
-
-// try {
-//   const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
-//   const jsonData = await d3.json(jsonurl);
-//   const stations = jsonData.data.stations;
-//   console.log('Stations Array:', stations);
-
-//   // Append circles for each station
-//   const circles = svg
-//     .selectAll('circle')
-//     .data(stations)
-//     .enter()
-//     .append('circle')
-//     .attr('r', 7)
-//     .attr('fill', 'steelblue')
-//     .attr('stroke', 'white')
-//     .attr('stroke-width', 1)
-//     .attr('opacity', 0.85);
-
-//   // Function to update positions
-//   function updatePositions() {
-//     circles
-//       .attr('cx', d => getCoords(d).cx)
-//       .attr('cy', d => getCoords(d).cy);
-//   }
-
-//   // Initial marker placement
-//   updatePositions();
-
-//   // Keep markers synced with map movements/zooms
-//   map.on('move', updatePositions);
-//   map.on('zoom', updatePositions);
-//   map.on('resize', updatePositions);
-//   map.on('moveend', updatePositions);
-
-// } catch (error) {
-//   console.error('Error loading JSON:', error);
-// }
-
-// });
-
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
@@ -156,10 +54,11 @@ map.on('load', async () => {
 
   const svg = d3.select('#map').select('svg');
   function getCoords(station) {
-    const point = new mapboxgl.LngLat(+station.Long, +station.Lat);
+    const point = new mapboxgl.LngLat(+station.lon, +station.lat);
     const { x, y } = map.project(point);
     return { cx: x, cy: y };
-  }
+}
+
 
   try {
     const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
@@ -173,7 +72,7 @@ map.on('load', async () => {
       .data(stations)
       .enter()
       .append('circle')
-      .attr('r', 7)
+      .attr('r', 5)
       .attr('fill', 'steelblue')
       .attr('stroke', 'white')
       .attr('stroke-width', 1)
